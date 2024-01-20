55d6cb344fbbb82afc83e93f5222becbe594ec63
sajid360 <ahteshamsajid8@gmail.com>
2024-01-21 01:23:07 +0600

    navbar and hero section added

---
+++ src/components/layout/Hero.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Right from "../icons/Right";
+
+export default function Hero() {
+    return (
+        <section className="flex items-center justify-between gap-x-5 my-12">
+            <div className="max-w-xl">
+                <h1 className="text-4xl font-semibold">You can have a bite of PIZZZZA</h1>
+                <p className="my-4 text-gray-500">Like you want PIZZZZA, on step further Like want PIZZZZA, on step further Like you, on step further. Like you want PIZZZZA, on step further. Like you want PIZZZZA, on step further</p>
+                <div className="flex gap-5 items-center">
+                    <button className="bg-primary px-5 py-2 text-white rounded-full flex gap-2">
+                        Order Now
+                        <Right />
+                    </button>
+                    <button className="flex gap-2 text-gray-500 font-semibold ">
+                        Learn More
+                        <Right />
+                    </button>
+                </div>
+            </div>
+
+            <div className="relative">
+                <Image src={"/pizza.png"} width="500" height="500" objectFit="contain" alt="Pizza"></Image>
+            </div>
+        </section>
+    );
+}
