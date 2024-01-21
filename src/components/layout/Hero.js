e71721b73532f12bcf5b614287707bcad439bd75
sajid360 <ahteshamsajid8@gmail.com>
2024-01-21 19:57:23 +0600

    home menu added

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -5,8 +5,8 @@ export default function Hero() {
     return (
         <section className="flex items-center justify-between gap-x-5 my-12">
             <div className="max-w-xl">
-                <h1 className="text-4xl font-semibold">You can have a bite of PIZZZZA</h1>
-                <p className="my-4 text-gray-500">Like you want PIZZZZA, on step further Like want PIZZZZA, on step further Like you, on step further. Like you want PIZZZZA, on step further. Like you want PIZZZZA, on step further</p>
+                <h1 className="text-6xl font-semibold leading-[70px]">You can have <br/> a bite of <span className="text-primary">PIZZZZA</span></h1>
+                <p className="my-6 text-gray-500">Like you want PIZZZZA, on step further Like want PIZZZZA, on step further Like you, on step further. Like you want PIZZZZA, on step further. Like you want PIZZZZA, on step further</p>
                 <div className="flex gap-5 items-center">
                     <button className="bg-primary px-5 py-2 text-white rounded-full flex gap-2">
                         Order Now
